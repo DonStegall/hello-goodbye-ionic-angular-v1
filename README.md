aae01eff95d14dc73c173ece92bdd0971384f8dc
DonStegall <don.f.stegall@gmail.com>
2022-04-22 23:55:34 -0400

    dropped in previous code .. see README.md

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
+
 # hello-goodbye-ionic-angular-v1
 
 ## the Hello Goodbye sample in Ionic with Angular and not much else
 
-Created using @ionic/cli@6.19.0
+Created using **@ionic/cli@6.19.0**
+
+code copied from existing **hello-goodbye-angular-ionic-v1** project .. see for history
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+  constructor(public alertController: AlertController)
+  {
+  }
+
+  private title = 'HomePage';
+
+  private textInput: string = null;
+
+  private async showInput() {
+    const alert = await this.alertController.create({
+      header: 'Input Contents',
+      subHeader: 'value of input field is:',
+      message: this.textInput,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  public handleHello() {
+    this.textInput = 'Hello';
+  }
+
+  public handleGoodbye() {
+    this.textInput = 'Goodbye';
+  }
+
+  public handleUpperCase() {
+    this.textInput = this.textInput.toUpperCase();
+  }
+
+  public handleLowerCase() {
+    this.textInput = this.textInput.toLowerCase();
+  }
+
+  public handleMessage() {
+    this.showInput();
+  }
+
+  public handleClear() {
+    this.textInput = null;
+  }
 
 }
